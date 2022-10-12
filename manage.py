01a040045b75a3fde3e71403ec44bf917e84fc39
Camille <camille@telescoop.fr>
2022-10-12 11:23:42 +0200

    Remettre à 0 les migrations

--- manage.py
+++ manage.py
@@ -4,9 +4,11 @@ import sys
 
 if __name__ == "__main__":
     if os.environ.get("TELESCOOP_DEV"):
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "labo.settings.dev")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vitrine_thierry.settings.dev")
     else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "labo.settings.production")
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE", "vitrine_thierry.settings.production"
+        )
 
     from django.core.management import execute_from_command_line
 
